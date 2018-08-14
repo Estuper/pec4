9ee85607bdda0530933296dcb6f1ee32f18435e6
estuper <francisco.javier.estuper@gmail.com>
2018-08-14 18:34:29 +0200

    subir

---
+++ example.php
@@ -0,0 +1,10 @@
+
+<?php 
+
+// Is there any input? 
+if( array_key_exists( "name", $_GET ) && $_GET[ 'name' ] != NULL ) { 
+    // Feedback for end user 
+    echo '<pre>Hello ' . $_GET[ 'name' ] . '</pre>'; 
+} 
+
+?> 
